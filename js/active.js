8b2404988521b030a07f7a6494ddc8b2be7210ca
me6edi <exited17@gmail.com>
2020-11-28 11:06:04 -0500

    owl carousel

---
+++ js/active.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    // $(".homepage-slides").owlCarousel({
+    //     items: 1,
+    //     nav:true,
+    //     loop: true,
+    //     autoplay: true,
+    //     dots: false,
+    //     loop:true,
+    //     navTxt:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"]
+
+    // });
+
+  });
