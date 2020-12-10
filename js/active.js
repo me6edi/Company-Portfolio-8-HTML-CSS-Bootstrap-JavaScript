134dd9fc25b82f59df1b5a574ba85c9d86f50b38
me6edi <exited17@gmail.com>
2020-12-10 09:55:24 -0500

    footer area complite

--- js/active.js
+++ js/active.js
@@ -9,6 +9,24 @@ $(document).ready(function(){
     //     navTxt:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"]
 
     // });
-    
+     
 
+    let $btns = $('.project-area .button-group button');
+    $btns.click(function(e){
+    $('.project-area .button-group button').removeClass('active');
+    e.target.classList.add('active');
+    
+    let selector = $(e.target).attr('data-filter');
+    $('.project-area .grid').isotope({
+      filter:selector
+      });
+    
+      return false;
+    
+     })
+
+
+
+     
+     
   });
